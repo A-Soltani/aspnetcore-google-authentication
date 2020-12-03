ae65e0f34bf760e8bd354526ad1984650c763cd8
Ali Soltani <soltani1384@gmail.com>
2020-12-03 14:16:20 +0330

    adding the API

---
+++ src/GoogleAuthentication.API/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace GoogleAuthentication.API.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
